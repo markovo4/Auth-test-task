49e282ff08110777683bec94e9554c782a734f24
markovo4 <obf2007@gmail.com>
2024-08-29 17:50:06 +0300

    added background

--- src/components/user/Login/Login.jsx
+++ src/components/user/Login/Login.jsx
@@ -1,9 +1,15 @@
 import styles from './styles.module.scss';
+import UnektMain from "../../../assets/svg/UnektMain.jsx";
+import Unekt from "../../../assets/svg/Unekt.jsx";
 
 const Login = () => {
     return (
         <section className={styles.login}>
+            <UnektMain/>
+            <Unekt/>
             <div className={styles.loginBackground}/>
+            <div className={styles.gtaMen}/>
+            <div className={styles.gtaWomen}/>
             <div className={styles.footer}/>
         </section>
     )
