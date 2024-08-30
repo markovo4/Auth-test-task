84cc78148fc6c63f1f83416b97f53a617a229a8c
markovo4 <obf2007@gmail.com>
2024-08-30 23:52:44 +0300

    form styles

--- src/components/UI/inputs/FormInput/FormInput.jsx
+++ src/components/UI/inputs/FormInput/FormInput.jsx
@@ -4,23 +4,36 @@ import styles from './styles.module.scss';
 const FormInput = () => {
     const [isFocused, setIsFocused] = useState(false);
     const [hasValue, setHasValue] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleChange = (e) => {
+        setError(!e.target.checkValidity());
+        setHasValue(e.target.value.length > 0);
+    }
+
+    const handleBlur = (e) => {
+        setIsFocused(false)
+        setHasValue(e.target.value.length > 0);
+        setError(!e.target.checkValidity());
+    }
 
     return (
         <div className={styles.inputGroup}>
             <label
-                className={`${styles.label} ${isFocused || hasValue ? styles.focusedLabel : ''}`}
+                className={`${styles.label} ${isFocused || hasValue ? styles.focusedLabel : ''} ${error ? styles.errorLabel : ''}`}
                 htmlFor="username"
             >
                 Username
             </label>
             <input
-                className={styles.input}
+                className={`${styles.input} ${error ? styles.errorInput : ''}`}
                 id="username"
                 name="username"
                 type="text"
                 onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                onChange={(e) => setHasValue(e.target.value.length > 0)}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                required
             />
         </div>
     );
