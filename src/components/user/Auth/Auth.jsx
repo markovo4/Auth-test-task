4818d83adb24c48cc92aabf8fbaa19e54818d5c1
markovo4 <obf2007@gmail.com>
2024-08-31 17:53:13 +0300

    refactored forms

--- src/components/user/Auth/Auth.jsx
+++ src/components/user/Auth/Auth.jsx
@@ -1,179 +1,98 @@
-import FormInput from "../../UI/inputs/FormInput";
-import styles from './styles.module.scss';
 import {useState} from "react";
+import FormInput from "../../UI/inputs/FormInput";
 import FormCheckBox from "../../UI/inputs/FormCheckBox";
 import FormButton from "../../UI/buttons/FormButton/index.js";
+import styles from './styles.module.scss';
 
 const Auth = () => {
+    const [activeForm, setActiveForm] = useState('login');
+    const [isChecked, setIsChecked] = useState(false);
 
-    const [login, setLogin] = useState(true);
-    const [register, setRegister] = useState(false);
-    const [recoverPass, setRecoverPass] = useState(false);
-    const [checked, setChecked] = useState(false)
+    const handleFormSwitch = (targetForm) => {
+        setActiveForm(targetForm);
+    };
 
-    const handleClick = (e) => {
-        const {name} = e.target;
-        if (name === 'registerButton') {
-            setLogin(!login)
-            setRegister(!register)
-        } else if (name === 'loginButton') {
-            setLogin(!login)
-            setRegister(!register)
-        } else if (name === 'backButton') {
-            setLogin(!login)
-            setRecoverPass(!recoverPass)
-        } else if (name === 'recoverPass') {
-            setLogin(!login)
-            setRecoverPass(!recoverPass)
-        }
-    }
+    const handleCheck = () => {
+        setIsChecked(!isChecked);
+    };
 
-    const handleCheck = (id) => {
-        if (id === 'rememberMe') {
-            setChecked(!checked)
-        }
-    }
-
-    return (
-        <div className={styles.container}>
-            {login && <form className={styles.formLogin}>
-                <FormInput
-                    name={'login'}
-                    id={'login'}
-                    label={'login'}
-                    text={'Логин'}
-                    type={'text'}
-                />
-
-                <FormInput
-                    name={'e-mail'}
-                    id={'e-mail'}
-                    label={'e-mail'}
-                    text={'E-mail'}
-                    type={'email'}
-                />
-
+    const formContent = {
+        login: (
+            <form className={styles.formLogin}>
+                <FormInput name="login" id="login" label="login" text="Логин" type="text"/>
+                <FormInput name="e-mail" id="e-mail" label="e-mail" text="E-mail" type="email"/>
                 <div className={styles.wrapper}>
-                    <FormCheckBox
-                        id={'rememberMe'}
-                        label={'Запомнить меня'}
-                        onChecked={handleCheck}
-                        isChecked={checked}
-                    />
-
+                    <FormCheckBox id="rememberMe" label="Запомнить меня" onChecked={handleCheck} isChecked={isChecked}/>
                     <button
                         id="recoverPass"
                         type="button"
                         name="recoverPass"
                         className={styles.buttonRecover}
-                        onClick={handleClick}
-                        aria-label="Forgot your password?">
+                        onClick={() => handleFormSwitch('recoverPass')}
+                        aria-label="Forgot your password?"
+                    >
                         забыли пароль?
                     </button>
                 </div>
-
-                <FormButton
-                    onClick={handleClick}
-                    text={'Войти'}
-                    label={'Log in'}
-                    type={'button'}
-                    name={'authorize'}
-                    id={'authorize'}
-                />
-
+                <FormButton id="authorize" type="button" name="authorize" onClick={() => {
+                }} label="Log in" text="Войти"/>
                 <button
                     id="registerButton"
                     type="button"
                     name="registerButton"
                     className={styles.buttonRegister}
-                    onClick={handleClick}
-                    aria-label="Register for an account">
+                    onClick={() => handleFormSwitch('register')}
+                    aria-label="Register for an account"
+                >
                     Нет аккаунта? Зарегистрируйтесь!
                 </button>
-
-            </form>}
-
-            {register && <form className={styles.formLogin}>
-                <FormInput
-                    name={'login'}
-                    id={'login'}
-                    label={'login'}
-                    text={'Логин'}
-                    type={'text'}
-                />
-                <FormInput
-                    name={'e-mail'}
-                    id={'e-mail'}
-                    label={'e-mail'}
-                    text={'E-mail'}
-                    type={'email'}
-                />
-                <FormInput
-                    name={'password'}
-                    id={'password'}
-                    label={'password'}
-                    text={'Придумайте пароль'}
-                    type={'password'}
-                />
-                <FormInput
-                    name={'passwordConfirm'}
-                    id={'passwordConfirm'}
-                    label={'passwordConfirm'}
-                    text={'Повторите пароль'}
-                    type={'password'}
-                />
-
-                <FormButton
-                    id={'register'}
-                    type={'button'}
-                    name={'register'}
-                    onClick={handleClick}
-                    label={'Sign up'}
-                    text={'Зарегистрироваться'}
-                />
-
+            </form>
+        ),
+        register: (
+            <form className={styles.formLogin}>
+                <FormInput name="login" id="login" label="login" text="Логин" type="text"/>
+                <FormInput name="e-mail" id="e-mail" label="e-mail" text="E-mail" type="email"/>
+                <FormInput name="password" id="password" label="password" text="Придумайте пароль" type="password"/>
+                <FormInput name="passwordConfirm" id="passwordConfirm" label="passwordConfirm" text="Повторите пароль"
+                           type="password"/>
+                <FormButton id="register" type="button" name="register" onClick={() => {
+                }} label="Sign up" text="Зарегистрироваться"/>
                 <button
                     id="loginButton"
                     type="button"
                     name="loginButton"
                     className={styles.buttonRegister}
-                    onClick={handleClick}
-                    aria-label="Authorize in an account">
+                    onClick={() => handleFormSwitch('login')}
+                    aria-label="Authorize in an account"
+                >
                     Уже есть аккаунт? Авторизуйтесь!
                 </button>
-            </form>}
-
-            {recoverPass && <form className={styles.formLogin}>
-                <FormInput
-                    name={'e-mail'}
-                    id={'e-mail'}
-                    label={'e-mail'}
-                    text={'Введите E-mail'}
-                    type={'email'}
-                />
-
-
-                <FormButton
-                    id={'recoverPass'}
-                    type={'button'}
-                    name={'recoverPass'}
-                    onClick={handleClick}
-                    label={'Access recover'}
-                    text={'Востановить доступ'}
-                />
-
+            </form>
+        ),
+        recoverPass: (
+            <form className={styles.formLogin}>
+                <FormInput name="e-mail" id="e-mail" label="e-mail" text="Введите E-mail" type="email"/>
+                <FormButton id="recoverPass" type="button" name="recoverPass" onClick={() => {
+                }} label="Access recover" text="Востановить доступ"/>
                 <button
                     id="backButton"
                     type="button"
                     name="backButton"
                     className={styles.buttonRegister}
-                    onClick={handleClick}
-                    aria-label="Go back">
+                    onClick={() => handleFormSwitch('login')}
+                    aria-label="Go back"
+                >
                     Назад
                 </button>
-            </form>}
+            </form>
+        )
+    };
+
+    return (
+        <div className={styles.container}>
+            {formContent[activeForm]}
         </div>
-    )
-}
+    );
+};
 
 export default Auth;
